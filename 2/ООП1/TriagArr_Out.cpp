e2137ee3774c92290316479a6099e8327069b1c0
bekk97 <bekk97@mail.ru>
2017-06-18 23:19:33 +0700

    Initial commit

---
+++ 2/ООП1/TriagArr_Out.cpp
@@ -0,0 +1,17 @@
+#include "TriagArr.h"
+
+void TriagArr::Out(ofstream &out) {
+	if (perepoln == true)
+		out << "Контейнер переполнен" << endl;
+	out << "Это треугольная матрица: " << endl;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+			out << Array[i][j] << "\t";
+		out << "\n";
+	}
+	out << endl;
+	prov = NULL;
+	perepoln = false;
+}
+
